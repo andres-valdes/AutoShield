0d33a224ed0bac7a1abda72326118e69469c6013
andres-valdes <avald198@fiu.edu>
2024-12-01 10:22:19 -0800

    even tinier

--- src/App.cs
+++ src/App.cs
@@ -1,5 +1,4 @@
 using BepInEx;
-using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
 
@@ -14,11 +13,9 @@ public class ReviveAllies : BaseUnityPlugin
     public const string Namespace = "Ratzu" + nameof(AutoShield);
 
     private Harmony _harmony;
-    public static ManualLogSource logger;
 
     private void Awake()
     {
-        logger = Logger;
         _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Guid);
     }
 
--- src/patches/Humanoid_Patch.cs
+++ src/patches/Humanoid_Patch.cs
@@ -9,9 +9,20 @@ public class Humanoid_Patch
     {
         public static void Postfix(Humanoid __instance, ref bool __result, ItemDrop.ItemData item)
         {
-            if (__result && item.m_shared.m_itemType == ItemDrop.ItemData.ItemType.OneHandedWeapon)
+            if (!__result || __instance.GetLeftItem() != null)
             {
-                ShieldEquipper.MaybeEquipShield(__instance, __result);
+                return;
+            }
+            if (item.m_shared.m_itemType == ItemDrop.ItemData.ItemType.OneHandedWeapon)
+            {
+                ItemDrop.ItemData shield = __instance.m_inventory
+                    .GetAllItems()
+                    .Find(i => i.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Shield);
+                if (shield == null)
+                {
+                    return;
+                }
+                __instance.EquipItem(shield, false);
             }
         }
     }
